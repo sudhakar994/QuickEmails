17b96a52c0525e98444d15937fca11795669e1e4
sksm <sksm@SUDHAKARAN>
2020-04-26 09:42:51 +0530

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickEmail.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+       
+    }
+}
