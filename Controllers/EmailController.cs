9080c011e477a0254003c83e06dc3065c777b485
sksm <sksm@SUDHAKARAN>
2020-05-03 19:01:13 +0530

    add contacts

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Mvc;
 using QuickEmail.Data.IRepository;
+using QuickEmail.Models;
 using QuickEmail.Utility;
 
 namespace QuickEmail.Controllers
@@ -28,6 +29,48 @@ namespace QuickEmail.Controllers
             return View();
         }
 
+        #region Contacts
+        /// <summary>
+        /// Contacts
+        /// </summary>
+        /// <returns></returns>
+
+        public IActionResult Contacts()
+        {
+
+            return View();
+        }
+
+        #endregion
+
+        #region _AddContacts
+        /// <summary>
+        /// _AddContacts
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult _AddContacts()
+        {
+            return PartialView();
+        }
+        #endregion
+
+        #region SaveContacts
+        /// <summary>
+        /// SaveContacts
+        /// </summary>
+        /// <param name="contacts"></param>
+        /// <returns></returns>
+
+            [HttpPost]
+        public JsonResult SaveContacts(Contacts contacts)
+        {
+
+
+            return Json(true);
+        }
+
+        #endregion
 
 
     }
---
+++ Models/ContactDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickEmail.Models
+{
+    public class ContactDetails
+    {
+        public List<Contacts> Contacts { get; set; }
+    }
+
+    public class Contacts
+    {
+        public Guid UserId { get; set; }
+
+        public long ContactId { get; set; }
+
+        public string ContactName { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string Country { get; set; }
+
+        public string UserDomain { get; set; }
+    }
+}
